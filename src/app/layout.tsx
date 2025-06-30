ed3e511b35b0f7c31a3b63cf3d5e35ed06a4b390
Luccas Amorim <117477920+Almightyluccas@users.noreply.github.com>
2025-06-30 13:09:16 -0300

    improved metadata for better seo and added keywords.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,15 +17,59 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
-  title: "Naval Special Warfare Group One",
-  description: "Elite tactical team in Arma 3, where immersive simulations meet real-world tactics.",
-  icons: {
-    icon: "/images/nswg1-emblem.png"
+  title: "Naval Special Warfare Group One | Arma 3 Milsim Unit",
+  description:
+    "Join Naval Special Warfare Group One — an elite Arma 3 milsim unit inspired by Navy SEAL Team 6. Experience tactical realism, teamwork, and immersive operations.",
+  keywords: [
+    "Arma 3",
+    "Milsim",
+    "Naval Special Warfare",
+    "SEAL Team 6",
+    "Tactical Simulation",
+    "Military Roleplay",
+    "NSWG1",
+    "Arma 3 Units",
+    "Special Forces Arma 3",
+    "Red Squadron",
+    "Arma",
+    "Arma Units",
+    "Seal Team"
+  ],
+  authors: [{ name: "Luccas Amorim" }],
+  creator: "Naval Special Warfare Group One",
+  publisher: "Naval Special Warfare Group One",
+  robots: "index, follow",
+  alternates: {
+    canonical: "https://nswg1.vercel.app",
   },
-  // verification: {
-  //   google: "EnWMu1qqVWr54rRvXV-fpgiSRg1U-f0S7npdZ4Oti8o"
-  // }
-}
+  openGraph: {
+    title: "Naval Special Warfare Group One | Arma 3 Milsim",
+    description:
+      "Elite Arma 3 milsim unit based on SEAL Team 6. Realistic training, authentic tactics, and immersive gameplay.",
+    url: "https://nswg1.vercel.app",
+    siteName: "NSWG1 Arma 3 Milsim",
+    images: [
+      {
+        url: "/images/nswg1-emblem.png",
+        width: 800,
+        height: 600,
+        alt: "NSWG1 Emblem",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Naval Special Warfare Group One | Arma 3 Milsim",
+    description:
+      "Join our elite Arma 3 milsim team inspired by SEAL Team 6. Intense training, real tactics, immersive gameplay.",
+    images: ["/images/nswg1-emblem.png"],
+    creator: "@YourTwitterHandle",
+  },
+  icons: {
+    icon: "/images/nswg1-emblem.png",
+  },
+};
 
 
 export default async function RootLayout({
