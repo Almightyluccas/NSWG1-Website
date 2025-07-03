649ad9bb37e744bf053f6e4c825bbe63a4dc5ddf
Luccas Amorim <117477920+Almightyluccas@users.noreply.github.com>
2025-07-02 21:17:41 -0300

    fixed sitemap to be nswg1.org not localhost

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 
 import { MetadataRoute } from 'next';
 
-const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://nswg1.org';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const publicPages = [
