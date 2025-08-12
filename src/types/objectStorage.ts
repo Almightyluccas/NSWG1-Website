c64260c4800269c3ca1cf5aa60fb9467db5326d8
almightyluccas <lamorim7657@outlook.com>
2025-08-11 21:35:51 -0300

    fixed issue with refresh_tokens

---
+++ src/types/objectStorage.ts
@@ -0,0 +1,8 @@
+export interface GalleryItem {
+  title: string;
+  altText: string;
+  description: string;
+  category: string;
+  unit: string;
+}
+export type UploadType = "profile" | "background" | "document" | "gallery"
